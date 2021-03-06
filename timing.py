ae52c1c0cab5ef6a3f78e85dd644523e88e6faa2
Dennis Shih <dennisshih89@gmail.com>
2021-03-05 22:02:46 -0800

    uploading 3rd file

---
+++ timing.py
@@ -0,0 +1,18 @@
+import time
+
+#decorator
+def calculate_time(func):
+	def wrapper():
+		start_time=time.time()
+		func()
+		end_time=time.time()
+		total_time=end_time-start_time
+		print(f"Total time {total_time}")
+	return wrapper()
+
+@calculate_time
+def time_sleep():
+	time.sleep(2)
+
+time_sleep=calculate_time(time_sleep)
+
